8d3fd22414521f72956b8ff8e64339c4de51efa5
ciszakdamian <ciszakdamian@gmail.com>
2021-01-07 19:35:40 +0100

    added orders page

---
+++ RestaurantMobileApp/RestaurantMobileApp/Views/OrdersPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using RestaurantMobileApp.Models;
+using RestaurantMobileApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantMobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrdersPage : ContentPage
+    {
+
+
+        RestService _restService;
+ 
+
+        public OrdersPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+            this.getTableList();
+        }
+
+        async void getTableList()
+        {
+
+            Table[] tableList = null;
+            tableList = await _restService.GetTableList();
+
+            /*test.Text = tableList.Length.ToString();*/
+            test.Text = tableList.GetType().ToString();
+
+
+
+
+        }
+    }
+}
